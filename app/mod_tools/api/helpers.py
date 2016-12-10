611dc62d1f9f7bfa4848e14c4f123c3e276149c5
Tam Vu Quang <vuquangtam1994@gmail.com>
2016-12-10 12:36:28 +0700

    Adding auto-generate route from module

--- app/mod_tools/api/helpers.py
+++ app/mod_tools/api/helpers.py
@@ -3,7 +3,9 @@ import os
 import importlib
 
 def getModule(path):
-    return filter(lambda x : x.endswith(".py"), [path.strip("./").replace("/", ".") + "." + module for module in os.listdir(path)])
+    return [module.replace(".py", "")
+            for module in filter(lambda x : x.find("__init__") == -1 and x.endswith(".py"),
+                                 [path.strip("./").replace("/", ".") + "." + module for module in os.listdir(path)])]
 
 def getFunctions(module):
     if isinstance(module, basestring):
--- app/mod_tools/controllers.py
+++ app/mod_tools/controllers.py
@@ -1,11 +1,21 @@
 from flask import Blueprint, request, send_file, jsonify
 from app.mod_tools.api.helpers import *
 import app.mod_tools.tools as tools
+
 mod_tools = Blueprint("tools", __name__, url_prefix="/tools")
+view_pool = {}
 
-for function in getFunctions(tools):
-    print function
+for module in getModule("./app/mod_tools/tools"):
+    functions = getFunctions(module)
+    for function in functions:
+        view = lambda func=function[1]: func(**request.values.to_dict())
+        view.__name__ = function[0]
+        view_pool[function[0]] = view
 
-@mod_tools.route('/')
-def remote():
-    return render_template('remote/remote.html')
+for key in view_pool:
+    value = view_pool[key]
+    mod_tools.add_url_rule("/" + key, view_func=value, methods=["GET", "POST"])
+
+@mod_tools.route("/")
+def index():
+    return "Welcome to Tools"
